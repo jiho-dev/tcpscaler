04fe2581eb9d9595a88b6e6f4b0c4ef16c12e30a
Jiho Jung <irongate@naver.com>
2020-08-27 14:22:13 +0900

    Add some of vm info

--- rcmd.sh
+++ rcmd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#client_ip="10.0.5.162 10.0.6.11 10.0.5.31 10.0.6.4 10.0.5.87 10.0.6.207 10.0.5.90 10.0.6.58 10.0.0.3 10.0.5.78"
-client_ip="10.0.5.63 10.0.5.59 10.0.5.151 10.0.5.83 10.0.5.42 10.0.0.10 10.0.5.69 10.0.6.222 10.0.6.138 10.0.6.62"
+
 server_host_ip="172.24.50.21"
-client_host_ip="172.24.50.21 172.24.50.19 172.24.50.20 172.24.52.20 172.24.52.21"
+client_host_ip="172.24.50.19 172.24.50.20 172.24.52.20 172.24.52.21"
 host_ip="$server_host_ip $client_host_ip"
+
+client_ip="10.0.5.63 10.0.5.59 10.0.5.151 10.0.5.42 10.0.0.10 10.0.5.69 10.0.6.138 10.0.6.62 10.0.6.222 10.0.6.196"
 
 run_test() {
 	for ip in ${client_ip}; do
@@ -35,7 +36,7 @@ stop_test() {
 }
 
 restart_server() {
-	ssh -F tf-ssh-bastion.conf ubuntu@bastion-01 "sudo killall tcpserver; ./run_server.sh"
+	ssh -F tf-ssh-bastion.conf ubuntu@bastion-01 "sudo killall tcpserver; sudo ./setup-server.sh; ./run_server.sh"
 }
 
 reset_ct() {
@@ -59,6 +60,13 @@ set_max_ct() {
 	done
 }
 
+show_ct() {
+	for ip in ${host_ip}; do
+		echo "Show host currnet ct: $ip"
+		ssh -F ~/ssh-keys/lab-eu-ssh.conf centos@$ip "cat /proc/sys/net/netfilter/nf_conntrack_count"
+	done
+}
+
 show_syslog() {
 	ips="$client_ip"
 	local line=3
@@ -77,6 +85,16 @@ show_free() {
 		i=$((i+1))
 		echo "** Show free on client $i : $ip **"
 		ssh -F tf-ssh-bastion.conf ubuntu@$ip "free"
+	done
+}
+
+show_host_free() {
+	local i=0
+
+	for ip in ${host_ip}; do
+		i=$((i+1))
+		echo "** Show free on client $i : $ip **"
+		ssh -F ~/ssh-keys/lab-eu-ssh.conf centos@$ip "free"
 	done
 }
 
@@ -114,15 +132,22 @@ show_iperf() {
 	local ips=$client_ip
 	local i=0
 
+	iperf_cmd="iperf -c 10.0.0.4  -t 30 -P 16"
+	echo "***** iperf cmd on every host: $iperf_cmd *****"
+	echo
+
 	for ip in ${ips}; do
 		i=$((i+1))
 		echo -n "Run client $i iperf: $ip - "
 
-#		scp -F tf-ssh-bastion.conf run_iperf.sh ubuntu@$ip:~/
-		ret=$(ssh -F tf-ssh-bastion.conf ubuntu@$ip "iperf -c 10.0.0.2  -t 30 -P 16 | grep SUM")
+		ret=$(ssh -F tf-ssh-bastion.conf ubuntu@$ip "$iperf_cmd | grep SUM")
 		perf=$(echo $ret | awk '{ print $6 }')
 		echo "$perf Gbps"
 	done
+}
+
+install_perf() {
+	ssh -F ~/ssh-keys/lab-eu-ssh.conf centos@172.24.50.21 "sudo yum -y install linux-tools-common perf linux-tools-generic"
 }
 
 #######################
